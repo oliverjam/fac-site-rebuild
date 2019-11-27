317801abf4854a258997aae075fca39ae4e94155
oliverjam <oliverjamesphillips@gmail.com>
2019-11-27 15:12:43 +0000

    Move shared html stuff to layout

---
+++ _includes/layouts/base.11ty.js
@@ -0,0 +1,32 @@
+const html = String.raw;
+
+module.exports = data => {
+  return html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      </head>
+      <body>
+        ${data.content}
+      </body>
+      <script
+        async
+        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+      ></script>
+      <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          });
+        }
+      </script>
+    </html>
+  `;
+};
--- home/home.11ty.js
+++ home/home.11ty.js
@@ -1,38 +1,18 @@
+const Markdown = require("markdown-it");
+const md = new Markdown();
+
 const html = String.raw;
 
 exports.data = {
   permalink: "/",
+  layout: "layouts/base",
 };
 
 exports.render = data => {
   console.log(data);
   return html`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      </head>
-      <body>
-        <h1>${data.title}</h1>
-        <p>${data.body}</p>
-      </body>
-      <script
-        async
-        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-      ></script>
-      <script>
-        if (window.netlifyIdentity) {
-          window.netlifyIdentity.on("init", user => {
-            if (!user) {
-              window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/";
-              });
-            }
-          });
-        }
-      </script>
-    </html>
+    <h1>${data.title}</h1>
+    <video width="100%" src="${data.video}" muted autoplay loop></video>
+    <p>${md.render(data.body)}</p>
   `;
 };
