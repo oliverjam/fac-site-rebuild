51a380cc88efab4ab9c6211c14a71fc7ae888281
oliverjam <oliverjamesphillips@gmail.com>
2020-01-18 21:56:54 +0000

    Add partner quotes to /hire
    Relates #15

--- pages/hire/hire.11ty.js
+++ pages/hire/hire.11ty.js
@@ -12,23 +12,39 @@ exports.data = {
 };
 
 exports.render = data => {
-  const { title, intro, image, partners } = data;
+  const { title, intro, image, quotes, partners } = data;
   return html`
     <h1>${title}</h1>
     <div class="intro">
       ${md.render(intro)}
     </div>
     <hr class="divider" />
-    <img src="${image}" width="960" height="540" alt="" />
+    <img src="${image}" class="circle-reveal" width="960" height="540" alt="" />
 
-    <section class="stack">
-      <h2>Our Partners</h2>
-      <div class="reel">
-        ${partners.map(Logo)}
+    <section class="full-width stripes" aria-label="Our Partners">
+      <div class="stack5">
+        <div class="reel" style="--gap: var(--space5)" tabindex="0">
+          ${quotes.map(Quote)}
+        </div>
+        <div class="reel">
+          ${partners.map(Logo)}
+        </div>
       </div>
     </section>
   `;
 };
+
+function Quote({ name, company, body }) {
+  return html`
+  <figure class="testimonial partner">
+    <figcaption>
+      <h3>${name}</h3>
+      <div>${company}</h3>
+    </figcaption>
+    <blockquote>${body}</blockquote>
+  </figure>
+`;
+}
 
 function Logo(src) {
   return html`
