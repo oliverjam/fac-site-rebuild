254a004ea84cbced5191668230d21ace423a5774
oliverjam <oliverjamesphillips@gmail.com>
2020-01-19 15:05:40 +0000

    Delete random extra copy of apply page

--- src/pages/marketing/marketing.11tydata.js
+++ src/pages/marketing/marketing.11tydata.js
@@ -1,4 +1,6 @@
 module.exports = {
   layout: "layouts/marketing",
+  // each pages URL should either be the page-specific override (from the exports.data)
+  // or just the folder name (e.g. apply/)
   permalink: ({ page }) => page.permalink || page.fileSlug + "/",
 };
